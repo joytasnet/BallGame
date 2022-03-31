6529dfcad776d8deb06cea1800ea7aec3f064056
fibuwimo <ray-rem-craim-requiem@ezweb.ne.jp>
2022-03-31 09:52:25 +0900

    秋田BallGame

--- AkitaMain.java
+++ AkitaMain.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class AkitaMain{
 	public static void main(String[] args){
 		int[] nums = new int[100];
-		for(int i =0;i<100;i++){
+		for(int i =1;i<=100;i++){
 			nums[i]=i;
 		}
 		for(int i =0;i<nums.length-1;i++){
